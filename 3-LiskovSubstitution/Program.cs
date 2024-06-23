9e8c4be81957e9dae79dafb2c248a73d111a34da
jeffersondavila <jeffersondavila16@gmail.com>
2024-06-23 16:46:30 -0600

    [ADD] Sustitucion de liskov, consiste en seguir una base cuando se reutilizan priezas del mismo codigo

--- 3-LiskovSubstitution/Program.cs
+++ 3-LiskovSubstitution/Program.cs
@@ -1,16 +1,15 @@
 ﻿using Liskov;
 
 CalculateSalaryMonthly(new List<Employee>() {
-    new EmployeeFullTime("Pepito Pérez", 160, 10),
-    new EmployeeContractor("Manuel Lopera", 180, 0)
+    new EmployeeFullTime("Pepito Pérez", 1, 5),
+    new EmployeeContractor("Manuel Lopera", 1)
 });
 
-void CalculateSalaryMonthly(List<Employee> employees) 
+void CalculateSalaryMonthly(List<Employee> employees)
 {
     foreach (var item in employees)
     {
-        decimal salary = item.CalculateSalary((item is EmployeeFullTime));
+        decimal salary = item.CalculateSalary();
         Console.WriteLine($"The {item.Fullname}'s salary is {salary}");
-        
     }
 }
